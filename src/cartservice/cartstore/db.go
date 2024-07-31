05d26510ab73ea2e254af809701a437aa1ac6fa0
Leandro Poroli <leandro.poroli@kurtosistech.com>
2024-07-31 16:41:14 -0300

    adding connect db retry

--- src/cartservice/cartstore/db.go
+++ src/cartservice/cartstore/db.go
@@ -8,6 +8,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"time"
 )
 
 type Db struct {
@@ -22,7 +23,11 @@ func NewDb(
 	port string,
 ) (*Db, error) {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, username, password, name, port)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	maxRetries := 5
+	initialBackoff := 1 * time.Second
+	backoffMultiplier := 2.0
+
+	db, err := retryConnect(dsn, maxRetries, initialBackoff, backoffMultiplier)
 	if err != nil {
 		return nil, errors.Wrap(err, fmt.Sprintf("An error occurred opening the connection to the database with dsn %s", dsn))
 	}
@@ -34,6 +39,33 @@ func NewDb(
 	return &Db{
 		db: db,
 	}, nil
+}
+
+func retryConnect(dsn string, maxRetries int, initialBackoff time.Duration, backoffMultiplier float64) (*gorm.DB, error) {
+	var (
+		err error
+		db  *gorm.DB
+	)
+	backoff := initialBackoff
+
+	for i := 0; i < maxRetries; i++ {
+		// Attempt to execute the operation
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			logrus.Debugf("An error occurred opening the connection to the database with dsn %s", dsn)
+		} else {
+			return db, nil
+		}
+
+		// Log the error and wait before retrying
+		logrus.Debugf("Attempt %d failed: %v\n", i+1, err)
+		time.Sleep(backoff)
+
+		// Increase backoff duration
+		backoff = time.Duration(float64(backoff) * backoffMultiplier)
+	}
+
+	return nil, fmt.Errorf("connection to db failed after %d retries: %w", maxRetries, err)
 }
 
 func (db *Db) Close() error {
