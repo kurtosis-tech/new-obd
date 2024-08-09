87ab1070626b63400a759c7bc516a1ae820a2ea2
lostbean <talktoedgar@gmail.com>
2024-08-09 19:20:59 -0300

    add nix env

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Go development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go
+          gopls
+          go-tools
+          golangci-lint
+        ];
+
+        shellHook = ''
+          echo "Go development environment loaded"
+          go version
+        '';
+      };
+    });
+}
