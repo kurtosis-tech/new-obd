1b87d31824d9c39b28f3c2a9b210218c84409d57
Leandro Poroli <leandro.poroli@kurtosistech.com>
2024-08-14 16:24:50 -0300

    using JSDELIVR

--- src/frontend/currencyexternalservice/servicefactory.go
+++ src/frontend/currencyexternalservice/servicefactory.go
@@ -2,15 +2,11 @@ package currencyexternalservice
 
 import (
 	"github.com/kurtosis-tech/new-obd/src/currencyexternalapi"
-	"github.com/kurtosis-tech/new-obd/src/currencyexternalapi/config/freecurrency"
 	"github.com/kurtosis-tech/new-obd/src/currencyexternalapi/config/jsdelivr"
 )
 
 func CreateService(apiKey string) *CurrencyExternalService {
 	primaryApi := currencyexternalapi.NewCurrencyAPI(jsdelivr.GetJsdelivrAPIConfig(apiKey))
-	secondaryApi := currencyexternalapi.NewCurrencyAPI(freecurrency.GetFreeCurrencyAPIConfig(apiKey))
-
-	service := NewService(primaryApi, secondaryApi)
-
+	service := NewService(primaryApi)
 	return service
 }
