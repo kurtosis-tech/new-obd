ab83947d88fb9ddf38fc1f08513b61038c50c12d
Tedi Mitiku <tedi.m52@gmail.com>
2024-08-15 14:51:45 -0400

    remove neondb plugin

--- src/cartservice/env.sh
+++
@@ -1,6 +0,0 @@
-export PORT=8090
-export DB_USERNAME=tedidb_owner
-export DB_PASSWORD=6TJ3sBMkQzcg
-export DB_HOST=ep-jolly-sun-a5ve8gh1.us-east-2.aws.neon.tech
-export DB_PORT=5432
-export DB_NAME=cart
